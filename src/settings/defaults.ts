a6348116d69bdc15b185c6de0cc6eadd3436d852
Anton <60476048+ASA-GH@users.noreply.github.com>
2026-07-24 01:28:47 +0700

    fix(defaults): use real newlines in excludePatterns (#1)

--- src/settings/defaults.ts
+++ src/settings/defaults.ts
@@ -13,7 +13,7 @@ export const DEFAULT_SETTINGS: GitEncryptSettings = {
 	branch: "main",
 	conflictAction: "ask",
 	encryptedMasterKey: "",
-	excludePatterns: ".obsidian\\n.trash\\ntemp.*",
+	excludePatterns: ".obsidian\n.trash\ntemp.*",
 	httpToken: "",
 	httpUsername: "",
 	localPath: ".git-encrypted",
